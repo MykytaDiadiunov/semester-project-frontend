8e6e8619f86582bc33244a6bf92e017d44cb08c2
AlesiaVsl <alesiashyrynskaya@gmail.com>
2026-01-21 15:22:53 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,16 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import { darkTheme, NConfigProvider, NMessageProvider } from 'naive-ui'
-import { useAppI18n } from '@/i18n';
+import { useAppI18n } from '@/i18n'
 
 const appI18n = useAppI18n()
 
 onMounted(async () => {
   appI18n.init()
 })
-
 </script>
 
-<style scoped scss>
-</style>
+<style scoped scss></style>
