bd963776793af31770ade894574a5a90adc3e50e
MykytaDiadiunov <myk23ta@gmail.com>
2026-01-27 01:19:08 +0200

    fix: get back "routes" after GOLOVA

--- src/composables/router.ts
+++ src/composables/router.ts
@@ -1,18 +1,28 @@
-import { useRouter } from "vue-router"
+import { useRouter } from 'vue-router'
 
 export const useRouting = () => {
   const router = useRouter()
 
   function back(): void {
     router.back()
-  } 
+  }
 
   function toHome(): void {
-    router.push({name: "Home"})
+    router.push({ name: 'Home' })
+  }
+
+  function toInstructions(): void {
+    router.push({ name: 'Instructions' })
+  }
+
+  function toLicenses(): void {
+    router.push({ name: 'Licenses' })
   }
 
   return {
     back,
     toHome,
+    toInstructions,
+    toLicenses,
   }
 }
